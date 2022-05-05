a77754866ccfdefd31ad637d41d026c178355360
adam_stableprice <adam@stableprice.com>
2022-05-05 09:23:54 +0100

    test fixed

--- src/tests/unit/test_config.py
+++ src/tests/unit/test_config.py
@@ -10,12 +10,10 @@ def test_development_config(test_app):
 
 def test_testing_config(test_app):
     test_app.config.from_object("src.config.TestingConfig")
-    assert test_app.config["SECRET_KEY"] == os.getenv("SECRET_KEY", "my_precious")
+    assert test_app.config["SECRET_KEY"] == "my_precious"
     assert test_app.config["TESTING"]
     assert not test_app.config["PRESERVE_CONTEXT_ON_EXCEPTION"]
-    assert test_app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get(
-        "DATABASE_TEST_URL"
-    )
+    assert test_app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get("DATABASE_TEST_URL")
 
 
 def test_production_config(test_app):
