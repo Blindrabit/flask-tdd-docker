014aebd55c74233ce089aafcd881af4e056d8b47
adam_stableprice <adam@stableprice.com>
2022-05-08 20:20:52 +0100

    admin panel added with tests

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,13 @@
 import os
 
 from flask import Flask
+from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the db
 db = SQLAlchemy()
+
+admin = Admin(template_mode="bootstrap3")
 
 
 def create_app(script_info=None):
@@ -14,12 +17,14 @@ def create_app(script_info=None):
     app.config.from_object(app_settings)
 
     db.init_app(app)
+    if os.getenv("FLASK_ENV") == "development":
+        admin.init_app(app)
 
     from src.api.ping import ping_blueprint
 
     app.register_blueprint(ping_blueprint)
 
-    from src.api.users import users_blueprint
+    from src.api.users.views import users_blueprint
 
     app.register_blueprint(users_blueprint)
 
